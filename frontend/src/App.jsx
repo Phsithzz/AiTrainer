0fc16e88e6d3e9ca940fa2e57f15d0da71c8e80e
Phsithzz <phoosith2548@gmail.com>
2026-04-12 15:44:45 +0700

    feature:set router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SelectPage from "./pages/SelectPage";
+import TrainPage from "./pages/TrainPage";
+import HistoryPage from "./pages/HistoryPage";
 const App = () => {
   return (
-    <div>App</div>
+     <BrowserRouter>
+      <div className="min-h-screen bg-[#0a0a0f] text-white font-mono overflow-x-hidden">
+        <Routes>
+          <Route
+            path="/"
+            element={<SelectPage  />}
+          />
+          <Route
+            path="/train/:exercise"
+            element={<TrainPage />}
+          />
+          <Route
+            path="/history"
+            element={<HistoryPage  />}
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
